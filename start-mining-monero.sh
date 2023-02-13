7b17c9c86cee657cbeebb3495b0191d3fc0fa82a
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-02-13 23:41:50 +0200

    Add files via upload

---
+++ start-mining-monero.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --enable-gpu --algorithm randomx --pool monero.herominers.com:1111 --wallet 42rCGFcQFHLLFSm8TvsqmbaUjX9ninvwaezs86VF36z32fgFuSEsYP1dFsR9AmfmGD3c2K5P43KhK5e6XJo5J5Ux7RcPNbf
