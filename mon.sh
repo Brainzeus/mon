10ee99d0a3674d00b2bd7270a57a2aaec0845f3c
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-07-16 20:57:14 +0300

    mon.sh

--- mon.sh
+++ mon.sh
@@ -3,4 +3,4 @@
 #This is an example you can edit and use
 #There are numerous parameters you can set, please check Help and Examples folder
 
-./SRBMiner-MULTI --enable-gpu --algorithm randomx --pool monero.herominers.com:1111 --wallet 43g1QZfrN4ejhZeDLSsrcgS8uxBizPwTzLHVNbcDsnD33t8z5fM6qjY4Q3dJ9r813GX4eDsNM5w1cKGjoM8qQK8893L8abV
+./SRBMiner-MULTI --algorithm minotaurx --pool stratum-eu.rplant.xyz:7068 --wallet RVB5jcbcTXPKiLLS2x9w19N6CdEF8vZEZz --gpu-boost 6
